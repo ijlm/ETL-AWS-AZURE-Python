fa6881b261a4b49e136d3d1958161c2245e45a84
ijlm <ivan.laverde.85@gmail.com>
2023-06-03 11:05:06 -0500

    "nuevos cambios"

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-import pandas as pd
-import tkinter as tk
-import matplotlib.pyplot as plt
 from pipepline_ETL import ejecutar_pipeline
+import sys
   
 
 ##Proceso de ETL
 ##posibles entrdadas :
 ### "Validacion" si se corre el ultimo archivo
 ### "Normal" recorre el directorio buscando archivos que cargar inciando desde la ultima carga"
-ejecutar_pipeline("Normal")
+
+ejecutar_pipeline(sys.argv[1])
     
 
-        
-        
        
--- pipepline_ETL.py
+++ pipepline_ETL.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 server = 'sql-server-test-analitica.database.windows.net'
 database = 'consultoria'
 username = 'consultoria'
-password = 'xxxxxxxx'
+password = 'Colombia23*'
 
 ##Extracción de informacion en partes 
 #primera parte busca los archivos a cargar lotes y configura la lista al proceso
@@ -70,35 +70,47 @@ def obtener_id_user(id_user):
         return 'NO'
 
 #proceso de load carga los datos a la BD
-def registrar_data(data):
+def registrar_data(data,cant,media,mini,maxi,tipo):
     try:        
         conn = pymssql.connect(server=server, user=username, password=password, database=database)
         cursor = conn.cursor()
         
+        j=0
+        price=0
+        n_max=maxi
+        n_min=mini
         for index, row in data.iterrows():
             
             query = "INSERT INTO TBL_PRICE_ID(fecha, price, user_id) \
                     VALUES (%s, %s, %s)"
             values = (row['timestamp'], row['price'], row['user_id'])
             cursor.execute(query, values)
+            conn.commit() 
             
             #existe=obtener_id_user(row['user_id'])                  
             #if existe=='NO': 
             #    query = "INSERT INTO cliente (ID, NOMBRE) \
             #    VALUES (%s, %s)"
             #    values = (row['user_id'], 'xxxx')
-            #    cursor.execute(query, values)        
-            conn.commit() 
+            #    cursor.execute(query, values)   
+            #      
             
-        #obtener_stadisticas()       
-        
+            price=price+row['price']
+            j=j+1 
+            n_max= maxi if row['price']<maxi else row['price']
+            n_min= mini if row['price']>mini else row['price']
+            obtener_stadisticas_poderadas(cant,media,n_min,n_max,j,price,tipo)  
+               
+            
+        #obtener_stadisticas()               
         conn.close()        
+        #return None 
         return "Registro exitoso"       
     except Exception as e:
         return f"Error de conexión: {str(e)}"
     
 ## funcion que extrae estadisticas 
-def obtener_stadisticas():
+def obtener_stadisticas(tipo):
     try:        
         conn = pymssql.connect(server=server, user=username, password=password, database=database)
         cursor = conn.cursor()
@@ -110,22 +122,41 @@ def obtener_stadisticas():
             count = row[0]
             avg_price = row[1]
             min_price = row[2]
-            max_price = row[3]
-            #return count, avg_price, min_price, max_price
+            max_price = row[3]         
+            avg_price= 0 if avg_price is None else avg_price   
+            min_price= 0 if min_price is None else min_price   
+            max_price= 0 if max_price is None else max_price   
+            print("############################ Datos En BD ########################################")
+            print("##### Cantidad:", count," | Promedio:",round(avg_price,2)," | Min Precio:",min_price," | Max precio:",max_price,"  #####") 
             print("###################################################################################")
-            print("##### count:", count," | Promedio:",round(avg_price,2)," | Min Precio:",min_price," | Max_precio:",max_price,"  #####") 
-            print("###################################################################################")
+            archivo = open("salidas/",tipo,".txt", "w")
+            archivo.write("############################ Datos En BD ########################################\n")
+            archivo.write("##### Cantidad:", count," | Promedio:",round(avg_price,2)," | Min Precio:",min_price," | Max precio:",max_price,"  #####\n")
+            archivo.write("###################################################################################\n")
+            archivo.close()
+
+            return count, avg_price, min_price, max_price
         else:
             #return 'NO'
             None
     except Exception as e:
         #return 'NO'    
         print(e)
-        
+
+#funcion que nos permite realizar calculos sin consultar la BD
+def obtener_stadisticas_poderadas(cant,media,mini,maxi,j,price,tipo):
+    try:
+        print("Calculo en Memoria; Cantidad:",(j+cant),"Media:",round(((price)+(cant*media))/(j+cant),2),"Min Precio",mini,"max Precio",maxi)
+        archivo = open(tipo,".txt", "w")
+        archivo.write("Calculo en Memoria; Cantidad:",(j+cant),"Media:",round(((price)+(cant*media))/(j+cant),2),"Min Precio",mini,"max Precio",maxi)
+        archivo.close()
+    except Exception as e:
+        #return 'NO'    
+        print(e)
+
          
 ## PIPELINE de ETL
-def ejecutar_pipeline(tipo):
-    obtener_stadisticas()
+def ejecutar_pipeline(tipo):    
     if tipo=='Normal':
         archivos=nombres_archivos_en_s3('aws-logs-793650758881-us-east-2', 'ETLPython')
         j=1
@@ -137,10 +168,11 @@ def ejecutar_pipeline(tipo):
                 #transforma la data 
                 df=transforma_df(df)
                 #Carga la data 
-                salida=registrar_data(df)
+                cant,media,mini,maxi=obtener_stadisticas(tipo)
+                salida=registrar_data(df,cant,media,mini,maxi,tipo)
                 print(salida)
                 j=j+1
-                obtener_stadisticas() 
+                 
             else :
                 print("no existe el consecutivo de carga")
             
@@ -152,10 +184,12 @@ def ejecutar_pipeline(tipo):
         #transforma la data 
         df=transforma_df(df)
         #Carga la data 
-        salida=registrar_data(df)
+        cant,media,mini,maxi=obtener_stadisticas(tipo)
+        salida=registrar_data(df,cant,media,mini,maxi,tipo)
         print(salida)
-        obtener_stadisticas()         
+                 
     else:
-        print("no es del proceso ")
+        print(tipo,"no es del proceso ")
+    obtener_stadisticas()
         
         
